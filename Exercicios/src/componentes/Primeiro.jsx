16681fe46f6cc5eb0b64b0f80d5621fcf71ecaba
Henrique França <henriqsf@gmail.com>
2022-12-09 09:34:49 -0300

    feat: Primeiro Componente

---
+++ Exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+
+//função com codigo jsx (sempre que usar um codigo jsx é preciso importar o react)
+export default function(){
+    return <h1>Primeiro</h1>
+}
--- Exercicios/src/index.js
+++ Exercicios/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './componentes/Primeiro'
+
 //função do react dom para renderisar elementos na pagina
-ReactDOM.render(<h1>React</h1>, document.getElementById('root')) // root vem de index.html da pasta public
+ReactDOM.render(<Primeiro />, document.getElementById('root')) // root vem de index.html da pasta public
 
 
 
