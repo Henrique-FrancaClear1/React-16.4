4df40e71ceffd3ed7c339c1714ee60f82b5350a8
Henrique França <henriqsf@gmail.com>
2022-12-09 09:25:13 -0300

    feat: Criando Projeto

---
+++ Exercicios/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//função do react dom para renderisar elementos na pagina
+ReactDOM.render(<h1>React</h1>, document.getElementById('root')) // root vem de index.html da pasta public
+
+
+
